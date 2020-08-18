bd2fff27d50e8ef44a1a39e15370a34176e8aa45
Samu Vartiainen <samuvartiainen@gmail.com>
2020-08-18 12:16:28 +0300

    test deploy to heroku

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
-require('./app.js');
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+
+app.use(express.static(__dirname + '/dist/react-todo-app'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/react-todo-app/index.html'));
 });
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+   var port = server.address().port;
+   console.log("Sovellus käynnissä portissa ", port)
+ });
